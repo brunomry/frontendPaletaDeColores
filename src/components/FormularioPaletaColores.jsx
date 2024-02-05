b0c54cf1089b75fccd318ec48ca09f8c9e555223
brunomry <brunomadozzo@gmail.com>
2024-02-05 02:47:43 -0300

    Agregue input de tipo color para seleccionar tambien de la paleta de colores. Al seleccionar un color, su valor se muestra en el input text.

--- src/components/FormularioPaletaColores.jsx
+++ src/components/FormularioPaletaColores.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, FormLabel } from "react-bootstrap";
 import TarjetasColores from "./TarjetasColores";
 
 const FormularioPaletaColores = () => {
@@ -27,10 +27,21 @@ const FormularioPaletaColores = () => {
       <section className="sectionForm mx-auto pt-5 pb-2 mb-5 rounded-3 shadow bg-white">
         <h2 className="text-center mb-5">Administrar colores</h2>
         <Form className="mb-5 d-flex flex-column" onSubmit={guardarColor}>
-          <Form.Group className="mb-4 formGroup d-flex flex-column gap-5 flex-md-row justify-content-center align-items-center py-5 px-2">
+          <Form.Group className="mb-5 formGroup d-flex flex-column gap-3 justify-content-center align-items-center py-5 px-2">
             <div className="bg-danger containerColor border border-2 border-dark"></div>
             <Form.Control
               type="text"
+              placeholder="Ingrese un color. Ej: green"
+              className="inputColor align-content-start py-3"
+              minLength={3}
+              maxLength={50}
+              onChange={(e) => setColor(e.target.value)}
+              value={color}
+              required
+            />
+            <FormLabel>También puede seleccionar un color: </FormLabel>
+             <Form.Control
+              type="color"
               placeholder="Ingrese un color. Ej: green"
               className="inputColor align-content-start py-3"
               minLength={3}
--- src/components/TarjetasColores.jsx
+++ src/components/TarjetasColores.jsx
@@ -3,7 +3,7 @@ import TarjetaColor from './TarjetaColor';
 
 const TarjetasColores = ({colores, borrarColor}) => {
   return (
-    <article className='d-flex containerCards justify-content-center flex-wrap gap-3 mx-auto'>
+    <article className='d-flex containerCards justify-content-center flex-wrap gap-3 mx-auto mt-5'>
       {colores.map((elementoColor, posicion) => (
           <TarjetaColor
             key={posicion}
